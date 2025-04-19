61feeb919c377a71eb778a6e784333cab50d8fb9
Renaud Bussieres <contact@renaudbussieres.com>
2025-04-19 09:31:57 -0400

    Unit 5 is done

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+       site: "https://bejewelled-longma-e7368b.netlify.app/"
+});
